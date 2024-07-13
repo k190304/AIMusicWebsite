0ce986c41e871c3b165ef9338f7ee4f29f9f6aa3
k190304 <102511379+k190304@users.noreply.github.com>
2024-07-13 12:02:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+song1_status = "";
+song2_status = "";
 
 function preload()
 {
@@ -33,6 +36,8 @@ function gotPoses(results)
     if (results.length > 0)
         {
             console.log(results);
+            scoreLeftWrist = results[0].pose.keypoints[9].score;
+            console.log("scoreLeftWrist = " + scoreLeftWrist);
 
             leftWristX = results[0].pose.leftWrist.x;
             leftWristY = results[0].pose.leftWrist.y;
@@ -46,4 +51,26 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    song1_status = song_1.isPlaying();
+    if (scoreLeftWrist < 0.2)
+        {
+            circle(leftWristX, leftWristY, 20);
+            song_2.stop();
+        }
+    
+    if (song1_status == false) 
+        {
+            song_1.play()
+            document.getElementById("song").innerHTML = "Song = " + song_1;
+        }   
+}
+
+function stop()
+{
+    song_1.stop();
+    song_2.stop();
 }
